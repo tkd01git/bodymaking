369143a75cb44346181d817ba4a21699a73aa11a
tkd01git <yuhei.takadaa@gmail.com>
2026-04-06 09:33:06 +0900

    Delete js directory

--- js/sampleData.js
+++
@@ -1,5 +0,0 @@
-
-window.sampleSetRecords = {
-  '2026-03-01__ベンチプレス': [{set:1,reps:8,weight:55},{set:2,reps:8,weight:55},{set:3,reps:7,weight:57.5}],
-  '2026-03-01__ラットプルダウン': [{set:1,reps:10,weight:40},{set:2,reps:10,weight:40},{set:3,reps:8,weight:42.5}]
-};

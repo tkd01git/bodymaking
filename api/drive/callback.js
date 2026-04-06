0b4ee1e53e07d3e8adb524dd11fbf25af47bfd32
tkd01git <yuhei.takadaa@gmail.com>
2026-04-06 13:10:47 +0900

    Update callback.js

--- api/drive/callback.js
+++ api/drive/callback.js
@@ -1,10 +1,12 @@
+import { getOAuth2Client } from '../_utils.js';
 
-import { getOAuth2Client } from './_utils.js';
+const CACHE_VERSION = '20260406-1';
 
 export default async function handler(req, res) {
   try {
     const url = new URL(req.url, `https://${req.headers.host}`);
     const code = url.searchParams.get('code');
+
     if (!code) {
       res.statusCode = 400;
       return res.end('Missing code');
@@ -17,10 +19,14 @@ export default async function handler(req, res) {
     res.setHeader('Set-Cookie', cookie);
 
     res.statusCode = 302;
-    res.setHeader('Location', '/');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    res.setHeader('Location', `/?v=${CACHE_VERSION}`);
     res.end();
   } catch (e) {
     res.statusCode = 500;
+    res.setHeader('Cache-Control', 'no-store');
     res.end(e.message);
   }
 }
